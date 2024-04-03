b2b334bf574a0cfb302fdc40148cb0381a064e22
Logan B Hajdukiewicz <122075213+LoganHajdukiewicz@users.noreply.github.com>
2024-04-03 14:12:57 -0400

    Took out audio file

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,10 +9,6 @@ const Layout = ({ children }) => {
   return (
     <div>
       {isLandingPage ? null : <Navbar />}
-
-      {/* Render the audio element with autoplay and loop attributes 
-      <audio src="/path/to/your/background-music.mp3" autoPlay loop /> */}
-
       <div style={{ paddingTop: isLandingPage ? 0 : "80px" }}>
         {children}
       </div>
